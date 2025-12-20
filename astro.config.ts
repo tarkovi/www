ebc141bafb27a0f063319070739522c34a380528
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2025-12-21 00:09:48 +0100

    tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,22 @@
 // @ts-check
 import sitemap from '@astrojs/sitemap'
-import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  experimental: {
+    fonts: [
+      {
+        cssVariable: '--font-inter',
+        name: 'Inter',
+        provider: fontProviders.google()
+      }
+    ]
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
