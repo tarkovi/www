d29c6687c89b67f1721d8268c7a748b3e79c82ac
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2025-12-24 23:34:24 +0100

    vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 // @ts-check
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  adapter: vercel(),
   integrations: [sitemap()],
   experimental: {
     fonts: [
